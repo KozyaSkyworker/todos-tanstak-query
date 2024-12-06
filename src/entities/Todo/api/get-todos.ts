ac5476216ac91ba248ee36fd58bb034f5ef3e09d
KozyaSkyworker <d_koz@inbox.ru>
2024-12-06 07:28:02 +0400

    feat: добавил задержку до получения todos

--- src/entities/Todo/api/get-todos.ts
+++ src/entities/Todo/api/get-todos.ts
@@ -1,5 +1,12 @@
 import { TodoDTO } from '../model';
 
 export const getTodos = () => {
-  return fetch('http://localhost:3000/todos').then((res) => res.json() as Promise<TodoDTO[]>);
+  return new Promise<TodoDTO[]>((resolve) => {
+    setTimeout(() => {
+      const data = fetch('http://localhost:3000/todos').then(
+        (res) => res.json() as Promise<TodoDTO[]>
+      );
+      resolve(data);
+    }, 2222);
+  });
 };
